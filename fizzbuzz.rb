32c21a49c56f71b0059cf75f4a607b3148368f65
Bence Monus <crome@moronia.hu>
2016-12-19 12:27:59 +0000

    Add manifest and config for connecting to Redis

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'pry'
 require_relative './lib/fizzbuzz_resolver'
-
+require 'redis'
+require 'json'
 
 class Fizzbuzz < Sinatra::Base
   get '/fizzbuzz/:number' do
@@ -10,6 +11,21 @@ class Fizzbuzz < Sinatra::Base
       FizzbuzzResolver.new.resolve(number.to_i).to_s
     else
       "The Fizzbuzz service isn't currently available"
+    end
+  end
+
+  get '/redis-status' do
+    vcap_services_config = JSON.parse(ENV['VCAP_SERVICES'])
+
+    host = vcap_services_config["rediscloud"].first["credentials"]["hostname"]
+    port = vcap_services_config["rediscloud"].first["credentials"]["port"]
+    password = vcap_services_config["rediscloud"].first["credentials"]["password"]
+
+    redis = Redis.new(host: host, port: port, password: password)
+    if redis.ping == "PONG"
+      "OKAY"
+    else
+      "FAIL"
     end
   end
 end
