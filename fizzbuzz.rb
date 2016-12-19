68990b08067fce884263c044b80a0a844ee0bb33
Bence Monus <crome@moronia.hu>
2016-12-19 11:54:26 +0000

    Only serve fizzbuzz when the service is enabled

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,11 @@ require_relative './lib/fizzbuzz_resolver'
 
 class Fizzbuzz < Sinatra::Base
   get '/fizzbuzz/:number' do
-    number = params[:number]
-    FizzbuzzResolver.new.resolve(number.to_i).to_s
+    if ENV['FIZZBUZZ_SERVICE_ENABLED'] == "true"
+      number = params[:number]
+      FizzbuzzResolver.new.resolve(number.to_i).to_s
+    else
+      "The Fizzbuzz service isn't currently available"
+    end
   end
 end
